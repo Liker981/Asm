4aa1131ff35b722c9125b3a7f88b7526f89a987c
Liker981 <103880458+Liker981@users.noreply.github.com>
2022-07-17 22:13:41 +0300

    Add files via upload
    working command intp

---
+++ command_intpr/working_commandintp.s
@@ -0,0 +1,40 @@
+	.file	"working_commandintp.s"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Do it: "
+.LC1:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movb	$1, -13(%rbp)
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L3:
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+.L2:
+	cmpb	$0, -13(%rbp)
+	jne	.L3
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	ret
